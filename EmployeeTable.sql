a6b792993b9083a39e3dc05282e662fbac0a6671
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-02-26 18:16:51 +0530

    commit

--- EmployeeTable.sql
+++ EmployeeTable.sql
@@ -29,75 +29,77 @@ Select * from emp;
 
 
 /*7.Write a SQL queries to display only emp_name and salary of all employees.*/
+select empname,sal from emp;
 
 
 /*8.Write a SQL query to display employees working in the 'IT' department.*/
-
+select * from emp where department='IT';
 
 
 /*9.Write a SQL query to display employees whose salary is greater than 30000.*/
-
+select * from emp where sal >30000;
 
 /*10.Write a SQL query to display employees who joined after '2022-01-01'.*/
+ select * from emp where join_date>'2020-01-01';
 
 /*11.Write a SQL query to display distinct department names from the Employee table.*/
-
+select distinct department from employee;
 
 /*12.Write a SQL query to display employees whose salary is between 25000 and 40000.*/
-
+select * from emp where sal between 25000 and 40000;
 
 /*13.Write a SQL query to display employees whose salary is not equal to 35000.*/
-
+select * from emp where sal!=35000;
 
 /*14.Write a SQL query to display employees who work in 'HR' or 'IT' department.*/
-
+select * from emp where department In('HR','IT');
 
 /*15.Write a SQL query to display employees whose name starts with letter 'P'.*/
-
+select * from emp where empname like 'P%';
 
 /*16.Write a SQL query to display employees whose salary is greater than or equal to 30000 and department is 'IT'.*/
-
+select * from emp where sal >= 30000 and department='IT';
 
 /*17.Write a SQL query to display employees whose salary is less than 30000 or department is 'Finance'.*/
-
+select * from emp where sal<=30000 or department='finance';
 
 /*18.Write a SQL query to display employees whose name contains the letter 'a'.*/
-
+Select * from emp where empname like '%a%';
 
 /*19.Write a SQL query to increase salary of all employees in the 'IT' department by 2000.*/
-
+update emp set sal = sal + 2000 where department = 'IT';
 
 /*20.Write a SQL query to update department of employee with emp_id 3 to 'HR'.*/
+update emp set department='HR' where empid=3;
+
+-- 21.Write a SQL query to update salary of employee named 'Amit' to 30000.*/
+update emp set sal=30000 where empname ='Amit';
+
+-- 22.Write a SQL query to change join_date of employee with emp_id 5 to '2024-01-01'.*/
+update emp set join_date='2024-01-01' where empid=5;
+
+-- 23.Write a SQL query to increase salary by 10% for employees in 'HR' department.*/
+update emp set sal= 26260 where department ='HR';
+select * from emp;
+
+-- 24.Write a SQL query to delete employee whose emp_id is 2.*/
+delete from emp where empid=2;
+
+-- 25.Write a SQL query to delete employees from 'Marketing' department.*/
+
+-- 26.Write a SQL query to delete employees whose salary is less than 25000.*/
 
 
-21.Write a SQL query to update salary of employee named 'Amit' to 30000.*/
+-- 27.Write a SQL query to find the total salary of all employees.*/
 
 
-22.Write a SQL query to change join_date of employee with emp_id 5 to '2024-01-01'.*/
+-- 28.Write a SQL query to find the average salary of employees in 'IT' department.*/
 
 
-23.Write a SQL query to increase salary by 10% for employees in 'HR' department.*/
+-- 29.Write a SQL query to find the maximum and minimum salary from the Employee table.*/
 
 
-24.Write a SQL query to delete employee whose emp_id is 2.*/
-
-
-25.Write a SQL query to delete employees from 'Marketing' department.*/
-
-
-26.Write a SQL query to delete employees whose salary is less than 25000.*/
-
-
-27.Write a SQL query to find the total salary of all employees.*/
-
-
-28.Write a SQL query to find the average salary of employees in 'IT' department.*/
-
-
-29.Write a SQL query to find the maximum and minimum salary from the Employee table.*/
-
-
-30.Write a SQL query to display all employees ordered by salary in descending order.*/
+-- 30.Write a SQL query to display all employees ordered by salary in descending order.*/
 
 31.Write a SQL query to display employees whose salary is greater than or equal to 30000 and less than or equal to 50000, ordered by salary ascending.*/
 
