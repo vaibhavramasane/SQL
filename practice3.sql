97c0cd6abc4378bdc4c73a9553513479e2e7b983
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-03-16 15:50:00 +0530

    final submission of the day

--- practice3.sql
+++ practice3.sql
@@ -1,1 +1,22 @@
--- 
+use gth_hub;
+
+-- 1. Find All Records
+select * from emp;
+
+-- 2. Display Employees With Salary Greater Than 50000
+select * from emp where sal>50000;
+
+-- 3. Sort Employees by Salary
+select * from emp order by sal desc;
+
+-- 4. Count Number of Employees
+select count(*)  from emp;
+
+-- 5. Find Maximum Salary
+select max(sal) from emp;
+
+-- 6. Find Minimum Salary
+-- 7. Find Average Salary
+-- 8. Find Total Salary
+-- 9. Find Employees Whose Name Starts With 'A'
+-- 10. Display Department Wise Salary
